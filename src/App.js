374912945df609f30b51a2e96e8946770af81fde
Naufil Shaikh <snaufil09@gmail.com>
2023-02-12 22:26:12 +0530

    Create structure for the app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-red-600-500'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
